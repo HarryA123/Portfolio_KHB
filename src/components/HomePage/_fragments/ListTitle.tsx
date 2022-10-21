2057d66561dde25cc05b3bfcd6de8054fc6c41d8
HarryA123 <harrykwak123@gmail.com>
2022-10-21 17:22:22 +0900

    feat: Create ListTitle Component

--- src/components/HomePage/_fragments/ListTitle.tsx
+++ src/components/HomePage/_fragments/ListTitle.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Button, Highlight, Text } from "@chakra-ui/react";
+import { Flex, Highlight, Text } from "@chakra-ui/react";
 import React from "react";
 
 interface ListTitleProps {
@@ -15,19 +15,6 @@ function ListTitle({ name, query }: ListTitleProps) {
           styles={{ py: "1", fontSize: "24px", color: "primary" }}>
           {name}
         </Highlight>
-      </Text>
-      <Text fontSize="24px" w="581px" color="white">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
-        condimentum placerat amet urna facilisis semper. Platea a, cras massa
-        imperdiet elit ut. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Purus condimentum placerat amet urna facilisis semper. Platea a,
-        cras massa imperdiet elit ut.Lorem ipsum dolor sit amet, consectetur
-        <br />
-        <br />
-        adipiscing elit. Purus semper. Platea a, cras massa imperdiet elit ut.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
-        condimentum placerat amet urna facilisis semper. Platea a, cras massa
-        imperdiet elit ut.
       </Text>
     </Flex>
   );
