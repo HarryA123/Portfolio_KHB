be23823370d85bdd8da44ac4562b11b7a0ba665f
HarryA123 <harrykwak123@gmail.com>
2022-10-21 17:13:25 +0900

    feat: Header Hover Effect

--- src/components/HomePage/HomeHeader.tsx
+++ src/components/HomePage/HomeHeader.tsx
@@ -1,18 +1,35 @@
 import { Flex, Button, Highlight, Text } from "@chakra-ui/react";
-import React from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import React, { useState } from "react";
+import Link from "next/link";
 import { Blog } from "../../../public/icons/Blog";
 import { Email } from "../../../public/icons/Email";
 import { Github } from "../../../public/icons/Github";
 import { Phone } from "../../../public/icons/Phone";
 
-function HomeHeader() {
+interface HomeHeaderProps {}
+
+function HomeHeader(props: HomeHeaderProps) {
+  const [emailShow, setEmailShow] = useState(false);
+  const [phoneShow, setPhoneShow] = useState(false);
+  const EmailOpen = () => {
+    setEmailShow(!emailShow);
+  };
+  const PhoneOpen = () => {
+    setPhoneShow(!phoneShow);
+  };
   return (
     <Flex
-      h="60px"
+      as="header"
+      h="80px"
       bg="black"
       alignItems="center"
       px="100px"
-      justifyContent="space-between">
+      w="100%"
+      pos="fixed"
+      zIndex="sticky"
+      justifyContent="space-between"
+      {...props}>
       <Flex gap="40px">
         <Text color="white">
           <Highlight query="01." styles={{ py: "1", color: "primary" }}>
@@ -36,10 +53,40 @@ function HomeHeader() {
         </Button>
       </Flex>
       <Flex gap="40px">
-        <Email />
-        <Phone />
-        <Github />
-        <Blog />
+        <Text display={!emailShow ? "none" : "inline"} color="primary">
+          harryKwak123@gmail.com
+        </Text>
+        <CopyToClipboard text="harryKwak123@gmail.com">
+          <Email
+            onClick={EmailOpen}
+            color={!emailShow ? "white" : "primary"}
+            transform={!emailShow ? "none" : "scale(1.3)"}
+            _hover={{ color: "primary", transform: "scale(1.3)" }}
+          />
+        </CopyToClipboard>
+        <Text display={!phoneShow ? "none" : "inline"} color="primary">
+          010-3913-4188
+        </Text>
+        <CopyToClipboard text="01039134188">
+          <Phone
+            onClick={PhoneOpen}
+            color={!phoneShow ? "white" : "primary"}
+            transform={!phoneShow ? "none" : "scale(1.3)"}
+            _hover={{ color: "primary", transform: "scale(1.3)" }}
+          />
+        </CopyToClipboard>
+        <Link href="https://github.com/HarryA123">
+          <Github
+            color="white"
+            _hover={{ color: "primary", transform: "scale(1.3)" }}
+          />
+        </Link>
+        <Link href="https://study-h.tistory.com/">
+          <Blog
+            color="white"
+            _hover={{ color: "primary", transform: "scale(1.3)" }}
+          />
+        </Link>
       </Flex>
     </Flex>
   );
